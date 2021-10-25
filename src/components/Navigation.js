86d83717299a0f824e91a5fcb9953ebe6f31d47e
Etienne Savary <etienne.savary@muvraline.fr>
2021-10-25 21:54:32 +0200

    completion des adresses personnelles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -56,25 +56,25 @@ const Navigation = () => {
             <div className="socialNetwork">
                 <ul>
                     <li>
-                        <a href="https://www.google.com" target="_blank"
+                        <a href="https://www.linkedin.com/in/etienne-s-b159a663/" target="_blank"
                            rel="noopener noreferrer">
                             <i className="fab fa-linkedin"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.google.com" target="_blank"
+                        <a href="https://github.com/Goldeaur" target="_blank"
                            rel="noopener noreferrer">
                             <i className="fab fa-github"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.google.com" target="_blank"
+                        <a href="https://twitter.com/DocteurSavary" target="_blank"
                            rel="noopener noreferrer">
                             <i className="fab fa-twitter"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.google.com" target="_blank"
+                        <a href="https://stackoverflow.com/users/16599418/etienne-savary" target="_blank"
                            rel="noopener noreferrer">
                             <i className="fab fa-stack-overflow"></i>
                         </a>
