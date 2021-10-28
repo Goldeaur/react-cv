efdc193abe4291fc9fa957978447bad46647e50f
Etienne Savary <etienne.savary@muvraline.fr>
2021-10-28 17:28:10 +0200

    completion data projets

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,56 +1,56 @@
 export const portfolioData = [
   {
     id: 1,
-    name: 'ipsum.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project1.JPG'
+    name: 'Curriculum Vitae',
+    languages: ['projet','javascript','css', 'react'],
+    languagesIcons: ['fab fa-js','fab fa-css3-alt', 'fab fa-react'],
+    source: 'https://github.com/Goldeaur/react-cv',
+    info: 'Code source de ce site réalisé avec react',
+    picture: './media/cv.JPG'
   },
   {
     id: 2,
-    name: 'lorem.fr',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project2.JPG '
+    name: 'Bibliothèque',
+    languages: ['projet','javascript','angular', 'java', 'postgreSQL', 'bootstrap'],
+    languagesIcons: ['fab fa-js','fab fa-angular','fab fa-java', 'fas fa-database', 'fab fa-bootstrap'],
+    source: 'https://github.com/Goldeaur/Bibliotheque',
+    info: "Conception d'une api et d'un client pour la gestion d'une petite bibliothèque municipale",
+    picture: './media/biblio.jpg '
   },
   {
     id: 3,
-    name: 'dolorset.fr',
-    languages: ['javascript','php','css'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project3.JPG'
+    name: 'La quête de Iasi',
+    languages: ['projet','write'],
+    languagesIcons: ['fas fa-pen-nib'],
+    source: 'https://www.simply-crowd.com/produit/la-quete-de-iasi/',
+    info: 'Contribution au Récit rédigé par un ami de Puteaux : Micha Cleef-Palmerr. Soutien, support, mise en page, auteur de la préface',
+    picture: './media/Recto-lasi.jpg'
   },
   {
     id: 4,
-    name: 'laboriosam.fr',
-    languages: ['javascript','php','react'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project4.JPG'
+    name: 'Vina : Les origines',
+    languages: ['projet','write'],
+    languagesIcons: ['fas fa-pen-nib'],
+    source: '',
+    info: "Roman fantasy en cours d'écriture. Une Jeune femme obtient le pouvoir de se métamorphoser en une adorable petite chatte blanche aux yeux bleus après avoir vendu son âme au diable par desespoir...",
+    picture: './media/vina.JPG'
   },
   {
     id: 5,
-    name: 'repudiante.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project5.JPG'
+    name: 'Voeu Parental',
+    languages: ['projet','music', 'write'],
+    languagesIcons: ['fas fa-music', 'fas fa-pen-nib'],
+    source: 'https://www.facebook.com/115031278562460/videos/478243640002202',
+    info: "Composition musicale d'une berceuse et rédaction des paroles",
+    picture: './media/soleil.jpg'
   },
   {
     id: 6,
-    name: 'test.io',
-    languages: ['javascript','react', 'css'],
-    languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project6.JPG'
+    name: 'Quiet-Night',
+    languages: ['projet','music','write', 'photos'],
+    languagesIcons: ['fas fa-music', 'fas fa-camera'],
+    source: 'https://www.facebook.com/115031278562460/videos/932675693464677',
+    info: 'Première composition accompagnée de photos prises de nuit à Montpellier en 2010',
+    picture: './media/night2.JPG'
   },
 ]
