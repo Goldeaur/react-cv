095883d0cd77f13406b749047ca14a7ea8e905f8
Goldeaur <34507362+Master-Goldeau@users.noreply.github.com>
2023-05-20 11:28:25 +0200

    MAJ Formations

--- src/components/Formation/Teach.js
+++ src/components/Formation/Teach.js
@@ -5,6 +5,18 @@ const Teach = () => {
         <div className="teach">
             <h3>Formations <br/> Reconversion Professionnelle</h3>
             <div className="teachContent">
+                <h4>AUBAY</h4>
+                <h4>Développeur BackEnd</h4>
+                <h5>2022-2023</h5>
+                <p>Architecture hexagonale, TDD, fonction sotckées PSQL, Spring Batch</p>
+            </div>
+            <div className="teachContent">
+                <h4>EOGILE</h4>
+                <h4>Développeur fullstack</h4>
+                <h5>2022</h5>
+                <p>Java 17- react</p>
+            </div>
+            <div className="teach">
                 <h4>GT'M ingénierie</h4>
                 <h4>Développeur fullstack</h4>
                 <h5>2018</h5>
