c0e7f1962bdd465333966d2e092adb2b7e907a00
Goldeaur <34507362+Master-Goldeau@users.noreply.github.com>
2023-05-20 11:12:34 +0200

    MAJ Languages.js

--- src/components/Knowledge/Languages.js
+++ src/components/Knowledge/Languages.js
@@ -4,13 +4,13 @@ import ProgressBar from "./ProgressBar";
 class Languages extends Component {
     state = {
         languages: [
-            {id:1, value:" Java ", xp:2, className:"fab fa-java"},
+            {id:1, value:" Java ", xp:3, className:"fab fa-java"},
             {id:2, value:" Javascript ", xp:0.5, className:"fab fa-js-square"},
             {id:3, value:" TypeScript ", xp:1, className:"fab fa-js-square"},
             {id:4, value:" Css ", xp:0.5, className:"fab fa-css3"}
         ],
         frameworks : [
-            {id:1, value:"Spring Boot", xp:2, className:"fas fa-leaf"},
+            {id:1, value:"Spring Boot", xp:3, className:"fas fa-leaf"},
             {id:2, value:"Spring Webflux", xp:0.8, className:"fas fa-leaf"},
             {id:7, value:"Maven", xp:3},
             {id:3, value:"Angular", xp:1, className:"fab fa-angular"},
